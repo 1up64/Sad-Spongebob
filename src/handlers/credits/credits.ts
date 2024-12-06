c06f8a5bfc8670fd8681f08d71cb3d1d0371cb02
1up <124456837+1up64@users.noreply.github.com>
2024-12-06 12:15:39 -0700

    ya

--- src/handlers/credits/credits.ts
+++ src/handlers/credits/credits.ts
@@ -4,15 +4,15 @@ import { HandlerDefinition } from '../../types'
 function Block(content: Pen<HTMLElement>[]): Pen<HTMLElement>[] {
     let pen = Pen.fromHTML(`<div>
                            <h1>developers</h1>
-                           <h2  class="divider" >Penguinify (maindev)</h2>
-                           <h1>contributors</h1>
-                           <h2>skrilll</h2>
-                           <h2 class="divider" >disnos9</h2>
+                           <h2  class="divider" >sad spongebob</h2>
+                           <h1>sad spongebob</h1>
+                           <h2>sad spongebob</h2>
+                           <h2 class="divider" >sad spongebob</h2>
 
                            <h1>icons</h2>
-                           <h3  class="divider" >font awesome</h3>
+                           <h3  class="divider" >saf spongebob</h3>
                            <h1>games</h2>
-                           <h3  class="divider" >selenite <a href="https://discord.gg/7jyufnwJNf" target="_blank">(discord)</a></h3>
+                           <h3  class="divider" >sad spongebob <a href="https://discord.gg/7jyufnwJNf" target="_blank">(discord)</a></h3>
                            </div>`)
     pen[0].setParent(content[0].element)
     return pen || []
